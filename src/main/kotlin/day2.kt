6b91020579e65af6faee8e8ee32ca9474dc0a9fc
Papp Bence <pappbence96@gmail.com>
2020-12-02 22:09:46 +0100

    Day 2 in one loop

--- src/main/kotlin/day2.kt
+++ src/main/kotlin/day2.kt
@@ -1,20 +1,17 @@
 import java.io.File
 
-fun main(args: Array<String>) {
-    val input = File( {}.javaClass.getResource("day2.txt").toURI())
-        .readLines()
-
-    val correctPasswords1 = input.count { line ->
-        val match = Regex("(\\d+)-(\\d+) ([a-z]): (\\w+)").find(line)!!
-        val (lowerBounds, upperBounds, char, pw) = match.destructured
-        pw.count { char[0] == it } in lowerBounds.toInt() .. upperBounds.toInt()
-    }
+fun main() {
+    var correctPasswords1 = 0
+    var correctPasswords2 = 0
+    File({}.javaClass.getResource("day2.txt").toURI())
+        .forEachLine {line ->
+            val match = Regex("(\\d+)-(\\d+) ([a-z]): (\\w+)").find(line)!!
+            val (lowerBounds, upperBounds, char, pw) = match.destructured
+            if (pw.count { char[0] == it } in lowerBounds.toInt() .. upperBounds.toInt())
+                correctPasswords1++
+            if ((pw[lowerBounds.toInt() - 1] == char[0]) xor (pw[upperBounds.toInt() - 1] == char[0]))
+                correctPasswords2++
+        }
     println("1. The number of correct passwords is $correctPasswords1")
-
-    val correctPasswords2 = input.count { line ->
-        val match = Regex("(\\d+)-(\\d+) ([a-z]): (\\w+)").find(line)!!
-        val (lowerBounds, upperBounds, char, pw) = match.destructured
-        (pw[lowerBounds.toInt() - 1] == char[0]) xor (pw[upperBounds.toInt() - 1] == char[0])
-    }
     println("2. The number of correct passwords is $correctPasswords2")
 }
